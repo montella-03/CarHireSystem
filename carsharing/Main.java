27f5781c92834ae67e5b515f73228aba49b02b23
montella-03 <oigoc60@gmail.com>
2022-10-31 23:04:25 +0300

    car hiring sys

--- carsharing/Main.java
+++ carsharing/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         ManagerMenu managerMenu = new ManagerMenu();
         managerMenu.mainMenu();
+        //adding a new company and you may list your companies.
     }
 }
